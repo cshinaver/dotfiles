1a9aa4d54ef44beca285e9ea27fd8f2fbe6b4c96
Charles Shinaver <bshinaver@gmail.com>
2014-12-25 23:47:31 -0500

    Added xschoolserver

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias homeserver="ssh -p 2222  bijan@$HOME_ADDRESS"
 alias schoolserver="ssh cshinave@$SCHOOL_ADDRESS"
 alias inventory="ssh -X charles@inlint.esc.nd.edu"
+alias xschoolserver="ssh -c arcfour,blowfish-cbc -XC cshinave@$SCHOOL_ADDRESS"
