1e527437374e14d357a99eed771c34f1e2fcd733
Charles Shinaver <bshinaver@gmail.com>
2015-01-18 15:59:06 -0500

    removed autojump sourcing in favor of custom sourcing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,3 @@ alias homeserver="ssh -p 2222  bijan@$HOME_ADDRESS"
 alias schoolserver="ssh cshinave@$SCHOOL_ADDRESS"
 alias inventory="ssh -X charles@inlint.esc.nd.edu"
 alias xschoolserver="ssh -c arcfour,blowfish-cbc -XC cshinave@$SCHOOL_ADDRESS"
-
-# Autojump
-[[ -s `brew --prefix`/etc/autojump.sh ]] && . `brew --prefix`/etc/autojump.sh
