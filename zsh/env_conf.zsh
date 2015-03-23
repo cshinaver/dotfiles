d457097bc6617461316e48a747b6df4be673ada3
Charles Shinaver <bshinaver@gmail.com>
2015-03-23 17:39:59 -0400

    Attaches to tmux automatically again

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -1,7 +1,7 @@
 # TMUX
 if which tmux 2>&1 >/dev/null; then
     #if not inside a tmux session, and if no session is started, start a new session
-    #test -z "$TMUX" && (tmux attach || mux start school )
+    test -z "$TMUX" && (tmux attach || tmux )
 fi
 
 # Source customrc
