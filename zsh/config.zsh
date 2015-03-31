d70b5c069bcacf67513c5566eadb8a705d139d20
Charles Shinaver <bshinaver@gmail.com>
2015-03-31 12:29:47 -0400

    Revert "renamed /Users/charlesshinaver/.dotfiles to"
    This reverts commit 6ff1ffe36fea9501d80bbbfbb401e050c4e951ba.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,9 +7,9 @@ fi
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($DOTFILES/functions $fpath)
+fpath=($ZSH/functions $fpath)
 
-autoload -U $DOTFILES/functions/*(:t)
+autoload -U $ZSH/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
