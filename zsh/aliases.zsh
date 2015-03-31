ec665323300a0aa82bdd4a5c102da5c0f4f93e75
Charles Shinaver <bshinaver@gmail.com>
2015-03-31 12:07:21 -0400

    Added tmux list sessions tl and attach ta

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,6 @@ alias inventory="ssh -X charles@inlint.esc.nd.edu"
 alias xschoolssh="sshpass -e ssh -c arcfour,blowfish-cbc -XC cshinave@$SCHOOL_ADDRESS"
 alias ryanpissh="ssh cshinaver@$RYANPI_ADDRESS"
 alias info="info --vi-keys"
+alias tl="tmux list-sessions"
+function ta {tmux attach -t $1} # Attach to tmux sessions
+
