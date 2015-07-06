cf4838f93404f51cf868ec68c532abdef79c3ede
Charles Shinaver <bshinaver@gmail.com>
2015-07-06 10:29:47 -0400

    Added VimClojure plugin with mappings

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -10,3 +10,6 @@ fi
 source ~/.customrc
 
 #export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+# Nailgun for VimClojure REPL
+export VIMCLOJURE_SERVER_JAR="$HOME/Dropbox/Programming/Clojure/server-2.3.6.jar"
