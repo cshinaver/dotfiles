3e3e4c39fe1020464fde92c1505e93c1adca230e
Charles Shinaver <bshinaver@gmail.com>
2015-08-04 01:18:24 -0400

    Tmux is not automatically started when terminal opened

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -1,11 +1,3 @@
-# TMUX
-if which tmux 2>&1 >/dev/null; then
-    #if not inside a tmux session, and if no session is started, start a new session
-    if [[ -z $SSH_CONNECTION ]] then
-        test -z "$TMUX" && (tmux attach || tmux )
-    fi
-fi
-
 # Source customrc
 source ~/.customrc
 
