5be35faf3376c84bbf5d3c441ecc12f7cf572662
cshinaver <bshinaver@gmail.com>
2015-11-06 17:21:49 -0500

    Combined dependencies

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,6 +1,13 @@
-{:user {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]]
+{:user {:dependencies [
+                       [org.clojars.gjahad/debug-repl "0.3.3"]
+                       [jonase/eastwood"0.2.1" :exclusions [org.clojure/clojure]]
+                       [cljfmt "0.3.0"]
+                       ]
         :injections [(use '[clojure.pprint :only [pp pprint]])
                      (use 'alex-and-georges.debug-repl)]
         :plugins [
                   [lein-ancient "0.6.7"]
+                  [lein-pprint "1.1.1"]
+                  [lein-kibit "0.1.2"]
+                  [venantius/ultra "0.3.4"]
                   ]}}
