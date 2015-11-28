5f50a484bfde53ac658c4be72d44ea94952fcba6
Charles Shinaver <bshinaver@gmail.com>
2015-11-28 15:24:25 -0500

    Added eastwood, cljfmt and kibit

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -1,6 +1,11 @@
-{:user {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]]
+{:user {:dependencies [
+                       [org.clojars.gjahad/debug-repl "0.3.3"]
+                       [jonase/eastwood "0.2.1" :exclusions  [org.clojure/clojure]]
+                       ]
         :injections [(use '[clojure.pprint :only [pp pprint]])
                      (use 'alex-and-georges.debug-repl)]
         :plugins [
                   [lein-ancient "0.6.7"]
+                  [lein-cljfmt "0.3.0"]
+                  [lein-kibit "0.1.2"]
                   ]}}
