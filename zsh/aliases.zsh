f2d2cbb7f9326cfc92e21b4098da99d2c174fafb
cshinaver <bshinaver@gmail.com>
2016-01-18 12:53:04 -0500

    Added needed spaces for zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,7 @@ alias xschoolssh="sshpass -e ssh -c arcfour,blowfish-cbc -XC cshinave@$SCHOOL_AD
 alias ryanpissh="ssh cshinaver@$RYANPI_ADDRESS"
 alias info="info --vi-keys"
 alias tl="tmux list-sessions"
-function ta {tmux attach -t $1} # Attach to tmux sessions
+function ta { tmux attach -t $1 } # Attach to tmux sessions
 alias b2d='eval "$(boot2docker shellinit)"'
 
 # Git stuff
@@ -18,7 +18,7 @@ alias dco="docker-compose"
 alias dcp="dco run --service-ports web"
 alias dcr="dco run web"
 alias dsh="docker run -v $(pwd):/kipwire_root -t -i --link db:db kipwire_web bash"
-function hrun {heroku ps:scale web=0 &> /dev/null; echo $1 > /dev/stdout; heroku ps:scale web=1 &> /dev/null}
+function hrun { heroku ps:scale web=0 &> /dev/null; echo $1 > /dev/stdout; heroku ps:scale web=1 &> /dev/null }
 
 autoload -U zmv
 alias mmv='noglob zmv -W' # Zsh multimove
