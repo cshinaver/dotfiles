e3fd7cdf88f555c91bf0ccf8c5ed08901b96fae5
cshinaver <bshinaver@gmail.com>
2016-03-19 04:01:22 -0400

    RVM sourcing is run if dir exists

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -1,6 +1,6 @@
 command -v virtualenvwrapper.sh &>/dev/null && source virtualenvwrapper.sh
 
-#export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+[ -d $HOME/.rvm ] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 # Nailgun for VimClojure REPL
 export VIMCLOJURE_SERVER_JAR="$HOME/Dropbox/Programming/Clojure/server-2.3.6.jar"
