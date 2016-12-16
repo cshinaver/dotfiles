f3221946fa89919c7d9ab41279cea63b05658882
cshinaver <bshinaver@gmail.com>
2016-12-16 14:14:34 -0500

    added auto ssh-agent configuring and sharing

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -1,6 +1,15 @@
 command -v virtualenvwrapper.sh &>/dev/null && source virtualenvwrapper.sh
 
-[ -d $HOME/.rvm ] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
 # Nailgun for VimClojure REPL
 export VIMCLOJURE_SERVER_JAR="$HOME/Dropbox/Programming/Clojure/server-2.3.6.jar"
+
+function start-ssh-agent () {
+    if ! pgrep -u $USER ssh-agent > /dev/null; then
+        ssh-agent > ~/.ssh-agent-thing
+    fi
+    if [[ "$SSH_AGENT_PID" == "" ]]; then
+        eval $(<~/.ssh-agent-thing) >/dev/null
+    fi
+}
+
+start-ssh-agent
