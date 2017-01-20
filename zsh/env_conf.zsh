b39eb8af49b91e3fa0c46999dd5b442b37fc1dd0
cshinaver <bshinaver@gmail.com>
2017-01-20 16:09:42 -0500

    SSH agent check no longer fails

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -5,9 +5,10 @@ export VIMCLOJURE_SERVER_JAR="$HOME/Dropbox/Programming/Clojure/server-2.3.6.jar
 
 function start-ssh-agent () {
     if ! pgrep -u $USER ssh-agent > /dev/null; then
+        echo "shouldn't do this"
         ssh-agent > ~/.ssh-agent-thing
     fi
-    if [[ "$SSH_AGENT_PID" == "" ]]; then
+    if [[ "$SSH_AGENT_PID" -eq "" ]]; then
         eval $(<~/.ssh-agent-thing) >/dev/null
     fi
 }
