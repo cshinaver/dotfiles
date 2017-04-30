7c3a294c754a9a5d344f20956a9f66193a6ba5dc
Charles Shinaver <bshinaver@gmail.com>
2017-04-30 19:40:54 -0400

    id_rsa added to ssh-agent at startup

--- zsh/env_conf.zsh
+++ zsh/env_conf.zsh
@@ -5,8 +5,8 @@ export VIMCLOJURE_SERVER_JAR="$HOME/Dropbox/Programming/Clojure/server-2.3.6.jar
 
 function start-ssh-agent () {
     if ! pgrep -u $USER ssh-agent > /dev/null; then
-        echo "shouldn't do this"
         ssh-agent > ~/.ssh-agent-thing
+        ssh-add
     fi
     if [[ "$SSH_AGENT_PID" -eq "" ]]; then
         eval $(<~/.ssh-agent-thing) >/dev/null
